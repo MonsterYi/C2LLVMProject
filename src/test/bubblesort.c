5d15653cac7b4be4d445887ea4843b882e0caeee
PowerOfEvil <52323616+PowerOfEvil614@users.noreply.github.com>
2020-12-30 00:13:54 +0800

    update

--- src/test/bubblesort.c
+++ src/test/bubblesort.c
@@ -8,13 +8,13 @@ int main() {
 	
 	printf("Please input array element:");
 	int i,j,k;
-	for (i = 0; i < len; i += 1){
+	for (i = 0; i < len; i = i + 1){
 		scanf("%d", &A[i]);
  	}
 	int temp = len - 1;
 	int temp1 = 0;
-	for (i = 0;i < len;i += 1) {
-		for (j = 0;j < temp;j += 1) {
+	for (i = 0;i < len;i = i + 1) {
+		for (j = 0;j < temp;j = j + 1) {
 			k = j + 1;
 			if (A[j] > A[k]) {
 				temp1 = A[j];
@@ -23,7 +23,7 @@ int main() {
 			}
 		}
 	}
-	for (i = 0; i < len; i += 1){
+	for (i = 0; i < len; i = i + 1){
 		printf("%d ", A[i]);
 	}
 	return 0;
